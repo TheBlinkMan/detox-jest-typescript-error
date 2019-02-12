ca9eb0d03e94960b67dfab167c6b6f8982a1b812
TheBlinkMan <cardosoalanf@gmail.com>
2019-02-12 18:43:50 -0200

    Add files to create error

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { detox, expect, by, element } from 'detox';
+
+describe('Example', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have welcome screen', async () => {
+    await expect(element(by.id('welcome'))).toBeVisible();
+  });
+});
